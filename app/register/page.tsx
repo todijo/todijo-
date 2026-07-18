9fd1866969b3fb8de6a7b9e71524ef11f48089fe
todijo <76115543+todijo@users.noreply.github.com>
2026-07-18 21:02:35 +0200

    Delete app directory

--- app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Suspense } from "react";
-import RegisterForm from "./RegisterForm";
-export default function RegisterPage(){
-  return <Suspense fallback={<main className="authPanel">Chargement…</main>}><RegisterForm /></Suspense>;
-}
