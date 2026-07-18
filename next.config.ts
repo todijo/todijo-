046dcb75cd91875d911179f325e78f68583a151e
todijo <76115543+todijo@users.noreply.github.com>
2026-07-18 17:09:20 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Forwarded-Host",
-            value: "todijo.com",
-          },
-          {
-            key: "X-Forwarded-Proto",
-            value: "https",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
