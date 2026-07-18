723247b79e03c261482693a4b20035046c48de4d
todijo <76115543+todijo@users.noreply.github.com>
2026-07-18 18:23:12 +0200

    Update route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -4,5 +4,8 @@ import { deleteSession } from "@/lib/session";
 export async function POST(request: Request) {
   await deleteSession();
 
-  return NextResponse.redirect(new URL("/login", request.url), 303);
+  return NextResponse.redirect(
+    new URL("/login", request.url),
+    { status: 303 }
+  );
 }
