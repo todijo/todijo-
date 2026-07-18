ae6865d62f52572ee2034ffdd8d0bc099dffcc7b
todijo <76115543+todijo@users.noreply.github.com>
2026-07-18 20:05:03 +0200

    Delete app directory

--- app/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { deleteSession } from "@/lib/session";
-
-export async function POST() {
-  await deleteSession();
-  return NextResponse.redirect("https://todijo.com/login", 303);
-}
